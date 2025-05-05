cdb17e44977367dd991c33847c85bbee5c44caf8
Vamsic045 <99563919+Vamsic045@users.noreply.github.com>
2025-05-05 23:42:49 +0530

    Update exchanges.py

--- exchanges.py
+++ exchanges.py
@@ -1,17 +1,9 @@
+from exchanges import get_price_binance, get_price_coindcx, get_price_bitget
 
-import ccxt
+ask_b, bid_b = get_price_binance()
+ask_c, bid_c = get_price_coindcx()
+ask_g, bid_g = get_price_bitget()
 
-def get_price_binance(symbol="BTC/USDT"):
-    exchange = ccxt.binance()
-    ticker = exchange.fetch_ticker(symbol)
-    return ticker['ask'], ticker['bid']
-
-def get_price_coindcx(symbol="BTC/USDT"):
-    exchange = ccxt.coindcx()
-    ticker = exchange.fetch_ticker(symbol)
-    return ticker['ask'], ticker['bid']
-
-def get_price_bitget(symbol="BTC/USDT"):
-    exchange = ccxt.bitget()
-    ticker = exchange.fetch_ticker(symbol)
-    return ticker['ask'], ticker['bid']
+print(f"Binance: Ask - {ask_b}, Bid - {bid_b}")
+print(f"CoinDCX: Ask - {ask_c}, Bid - {bid_c}")
+print(f"Bitget: Ask - {ask_g}, Bid - {bid_g}")
