d56d152d38985c14b809be5b905a4967231a4d0b
Vamsic045 <99563919+Vamsic045@users.noreply.github.com>
2025-05-05 23:35:42 +0530

    Update exchanges.py

--- exchanges.py
+++ exchanges.py
@@ -1,1 +1,17 @@
 
+import ccxt
+
+def get_price_binance(symbol="BTC/USDT"):
+    exchange = ccxt.binance()
+    ticker = exchange.fetch_ticker(symbol)
+    return ticker['ask'], ticker['bid']
+
+def get_price_coindcx(symbol="BTC/USDT"):
+    exchange = ccxt.coindcx()
+    ticker = exchange.fetch_ticker(symbol)
+    return ticker['ask'], ticker['bid']
+
+def get_price_bitget(symbol="BTC/USDT"):
+    exchange = ccxt.bitget()
+    ticker = exchange.fetch_ticker(symbol)
+    return ticker['ask'], ticker['bid']
