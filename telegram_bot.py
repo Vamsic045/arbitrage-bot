a37018779b85e6f96c68a32760cf5a2dde75f34b
Vamsic045 <99563919+Vamsic045@users.noreply.github.com>
2025-05-07 20:58:30 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,7 +5,7 @@ from config import TOP_100_COINS
 from trusted_exchanges import get_top_100_trusted_exchanges
 
 EXCHANGES = get_top_100_trusted_exchanges()
-YOUR_CHAT_ID = "YOUR_CHAT_ID_HERE"  # Replace with your actual Telegram chat ID
+YOUR_CHAT_ID = "6738389946"  # Replace with your actual Telegram chat ID
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("🚀 Welcome to Arbitrage Bot! You’ll receive updates every minute.")
@@ -28,7 +28,8 @@ def send_opportunities(context: CallbackContext):
     context.bot.send_message(chat_id=context.job.context, text=message)
 
 def setup_bot():
-    updater = Updater("YOUR_TELEGRAM_BOT_TOKEN", use_context=True)
+    updater = Updater("7802388978:AAGs1K461PsA6CgzqXaucWj5HkpCZJfApyI", use_context=True)
+
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     updater.job_queue.run_repeating(send_opportunities, interval=60, first=10, context=YOUR_CHAT_ID)
